2e63ba4be796ab8e820d8d5575f4bb719cc87ffe
Harsh Parekh <58972156+Harsh-Parekh@users.noreply.github.com>
2020-03-02 17:40:39 +0530

    Create Triangle.java
    if user enter n=5 
    *
    **
    ***
    ****
    *****
    these will be output

---
+++ Triangle.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Triangle
+{
+  public static void main(String args)
+  {
+    Scanner o=new Scanner(System.in);
+    int n;
+    System.out.print("Enter no of rows :");
+    n=o.nextInt();
+    for (int i=0;i<n;i++)
+    {
+      for(int j=0;j<=i;j++)
+      {
+        System.out.print("*");
+      }
+      System.out.println("");
+    }
+  }
+ }
