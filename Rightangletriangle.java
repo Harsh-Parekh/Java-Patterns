40767b7493500c13efedacad44d089b095554007
Harsh Parekh <58972156+Harsh-Parekh@users.noreply.github.com>
2020-03-09 18:36:23 +0530

    Update Rightangletriangle.java

--- Rightangletriangle.java
+++ Rightangletriangle.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 class Triangle
 {
-  public static void main(String args)
+  public static void main(String args[])
   {
     Scanner o=new Scanner(System.in);
     int n;
